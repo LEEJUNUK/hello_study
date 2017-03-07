772e67337fd49004857db958316d2338992397d3
LEEJUNUK <junukwnsdnr@naver.com>
2017-03-07 10:40:43 +0900

    Update Hello_Test.java
    last add

--- src/hello_study/Hello_Test.java
+++ src/hello_study/Hello_Test.java
@@ -7,5 +7,6 @@ public class Hello_Test {
 		System.out.println("Hello Two Test");
 		System.out.println("로컬에서 추가");
 		System.out.println("git Hub Edit");
-	}
+                  Sysytem.out.println("lastla");			
+}
 }
